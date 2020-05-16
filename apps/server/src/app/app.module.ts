2ef5fb00b604dc0af480865b271eda700019d924
Jay Bell <jaycooperbell@gmail.com>
2020-05-15 17:01:32 -0700

    added mongoose and nest router and change to small resource class

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RouterModule } from 'nest-router';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('', {}), RouterModule.forRoutes([])],
   controllers: [AppController],
   providers: [AppService]
 })
